4fde28f53056a3a3aaf5443f276ad5ef35907fb0
Natalie Smith <nsmith@hashicorp.com>
2024-03-11 11:44:47 -0700

    chore: add registry types for ember style modifier

--- packages/components/src/types/global.d.ts
+++ packages/components/src/types/global.d.ts
@@ -5,9 +5,15 @@ declare module 'ember-truth-helpers';
 import { LinkTo } from "@ember/routing";
 
 import type EmberTruthRegistry from 'ember-truth-helpers/template-registry';
+import type EmberElementHelperRegistry from 'ember-element-helper/template-registry';
+import type EmberStyleModifier from 'ember-style-modifier';
+
+export default interface EmberStyleModifierRegistry {
+  style: typeof EmberStyleModifier;
+}
 
 declare module '@glint/environment-ember-loose/registry' {
-  export default interface Registry extends EmberTruthRegistry /*, other addon registries */ {
+  export default interface Registry extends EmberTruthRegistry, EmberElementHelperRegistry, EmberStyleModifierRegistry /*, other addon registries */ {
     // local entries
     "LinkToExternal": typeof LinkTo;
   }
