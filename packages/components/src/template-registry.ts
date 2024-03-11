2b4c9f2835f2fae411887bbf600ec251d0fcb79e
Natalie Smith <nsmith@hashicorp.com>
2024-03-11 12:50:36 -0700

    chore: add template-registry entries for text components

--- packages/components/src/template-registry.ts
+++ packages/components/src/template-registry.ts
@@ -6,9 +6,17 @@
 import type HdsButtonIndexComponent from './components/hds/button';
 import type HdsDismissButtonIndexComponent from './components/hds/dismiss-button';
 import type HdsInteractiveIndexComponent from './components/hds/interactive';
+import type HdsTextIndexComponent from './components/hds/text';
+import type HdsTextBodyComponent from './components/hds/text/body';
+import type HdsTextDisplayComponent from './components/hds/text/display';
+import type HdsTextCodeComponent from './components/hds/text/code';
 
 export default interface HdsComponentsRegistry {
   HdsButtonComponent: typeof HdsButtonIndexComponent;
   HdsDismissComponent: typeof HdsDismissButtonIndexComponent;
   HdsInteractiveComponent: typeof HdsInteractiveIndexComponent;
+  HdsText: typeof HdsTextIndexComponent;
+  HdsTextBody: typeof HdsTextBodyComponent;
+  HdsTextDisplay: typeof HdsTextDisplayComponent;
+  HdsTextCode: typeof HdsTextCodeComponent;
 }
