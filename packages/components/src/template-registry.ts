b85a7e83bf224e5e8e8b08007a593342e9b4a626
Chris Hut <tophernuts@gmail.com>
2024-03-13 11:41:44 -0700

    refactor: Convert badge to Typescript

--- packages/components/src/template-registry.ts
+++ packages/components/src/template-registry.ts
@@ -6,9 +6,11 @@
 import type HdsButtonIndexComponent from './components/hds/button';
 import type HdsDismissButtonIndexComponent from './components/hds/dismiss-button';
 import type HdsInteractiveIndexComponent from './components/hds/interactive';
+import type HdsBadgeIndexComponent from "./components/hds/badge";
 
 export default interface HdsComponentsRegistry {
   HdsButtonComponent: typeof HdsButtonIndexComponent;
   HdsDismissComponent: typeof HdsDismissButtonIndexComponent;
   HdsInteractiveComponent: typeof HdsInteractiveIndexComponent;
+  HdsBadgeComponent: typeof HdsBadgeIndexComponent;
 }
